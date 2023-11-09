aaa45dff2841a78d7ae66b797c52d4f2e3c82348
George Spiller <geospiller@gmail.com>
2023-11-09 15:14:16 +0000

    Added skeleton ASPNET project

---
+++ ASPNET REST API/ValidationModelAPI/Controllers/ValuationController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ValuationModel;
+
+
+[ApiController]
+[Route("[controller]")]
+public class ValuationController: ControllerBase
+{
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<Vessel> Get()
+    {
+
+    }
+}
